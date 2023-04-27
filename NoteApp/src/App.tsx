6198919bf2b8821a2dc7e63bbd3bdc578454efcc
Konrad Frączak <s20178@pjwstk.edu.pl>
2023-04-28 00:28:45 +0200

    build

--- NoteApp/src/App.tsx
+++ NoteApp/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { NewNote } from "./Components/NewNote";
 
-function App() {
-  const [count, setCount] = useState(0)
+export type Note = {
+	id: string
+} & NoteData
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export type NoteData = {
+	title: string
+	markdown: string
+	tags: Tag[]
+} 
+export type Tag = {
+	id: string
+	label: string
 }
 
-export default App
+function App() {
+	return (
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Home</h1>}></Route>
+				<Route path="/new" element={<NewNote/>}></Route>
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="*" element={<Navigate to="/" />}></Route>
+			</Routes>
+		</Container>
+	);
+}
+
+export default App;
