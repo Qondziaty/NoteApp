56992e94e076d15f6a87e547b53abc47ee708f17
Konrad Frączak <s20178@pjwstk.edu.pl>
2023-05-04 22:47:37 +0200

    functionality update

--- NoteApp/src/Components/NoteForm.tsx
+++ NoteApp/src/Components/NoteForm.tsx
@@ -3,19 +3,26 @@ import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import CreatbleReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "../App";
-import {v4 as uuidV4} from "uuid"
+import { v4 as uuidV4 } from "uuid";
 
 type NoteFormProps = {
-	onSubmit: (data: NoteData) => void
-	onAddTag: (tag: Tag) => void
-	availableTags: Tag[]
-};
+	onSubmit: (data: NoteData) => void;
+	onAddTag: (tag: Tag) => void;
+	availableTags: Tag[];
+} & Partial<NoteData>;
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+export function NoteForm({
+	onSubmit,
+	onAddTag,
+	availableTags,
+	title = "",
+	markdown = "",
+	tags = [],
+}: NoteFormProps) {
 	const titleRef = useRef<HTMLInputElement>(null);
 	const markdownRef = useRef<HTMLTextAreaElement>(null);
-	const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-	const navigate = useNavigate() 
+	const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+	const navigate = useNavigate();
 
 	function handleSubmit(e: FormEvent) {
 		e.preventDefault();
@@ -25,7 +32,7 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
 			markdown: markdownRef.current!.value,
 			tags: selectedTags,
 		});
-		navigate("..")
+		navigate("..");
 	}
 	return (
 		<Form onSubmit={handleSubmit}>
@@ -34,23 +41,23 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
 					<Col>
 						<Form.Group controlId="title">
 							<Form.Label>Title</Form.Label>
-							<Form.Control ref={titleRef} required />
+							<Form.Control ref={titleRef} required defaultValue={title} />
 						</Form.Group>
 					</Col>
 					<Col>
 						<Form.Group controlId="tags">
 							<Form.Label>Tags</Form.Label>
 							<CreatbleReactSelect
-								onCreateOption={label => {
-									const newTag = {id: uuidV4(), label}
-									onAddTag(newTag)
-									setSelectedTags(prev => [...prev, newTag])
+								onCreateOption={(label) => {
+									const newTag = { id: uuidV4(), label };
+									onAddTag(newTag);
+									setSelectedTags((prev) => [...prev, newTag]);
 								}}
 								value={selectedTags.map((tag) => {
 									return { label: tag.label, value: tag.id };
 								})}
-								options={availableTags.map(tag => {
-									return{ label: tag.label, value: tag.id}
+								options={availableTags.map((tag) => {
+									return { label: tag.label, value: tag.id };
 								})}
 								onChange={(tags) => {
 									setSelectedTags(
@@ -66,7 +73,13 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
 				</Row>
 				<Form.Group controlId="markdown">
 					<Form.Label>Input</Form.Label>
-					<Form.Control required as="textarea" ref={markdownRef} rows={7} />
+					<Form.Control
+						defaultValue={markdown}
+						required
+						as="textarea"
+						ref={markdownRef}
+						rows={7}
+					/>
 				</Form.Group>
 				<Stack direction="horizontal" gap={2} className="justify-content-end">
 					<Button type="submit" variant="primary">
